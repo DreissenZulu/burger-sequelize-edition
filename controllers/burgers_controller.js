26a0272af359938dbd3248c39894dad02653b0ac
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-26 21:06:38 -0500

    Rewrote routes to use sequelize methods

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,31 +1,28 @@
 const express = require("express");
 // Import the burger module to access database functions
-const burger = require("../models/burger.js");
+const db = require("../models");
 
 // The router is like using app = express(), where the server is defined in another file
 let router = express.Router();
 
-router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
+router.get("/", (req, res) => {
+    db.burgers.findAll({}).then( data => {
         let burgerObj = {
             burger: data
-        };
-        console.log(burgerObj);
+        }
         res.render("index", burgerObj);
     })
 });
 
-router.post("/api/burgers", function (req, res) {
-    burger.insertOne(req.body.name, function (result) {
+router.post("/api/burgers", (req, res) => {
+    db.burgers.create({burgerName: req.body.name}).then( result => {
         res.json({ id: result.insertId });
     })
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-    let condition = `id=${req.params.id}`;
-    console.log(`Condition: ${condition}`);
-
-    burger.updateOne("devoured=true", condition, function (result) {
+router.put("/api/burgers/:id", (req, res) => {
+    db.burgers.update({devoured: true}, {where: {id: req.params.id} })
+    .then(result => {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
