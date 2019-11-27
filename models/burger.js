78bf6b70aba403184a95325c73b6be7ba1064200
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-26 21:08:25 -0500

    Removed unnecessary function, export the burger schema with error checking

--- models/burger.js
+++ models/burger.js
@@ -1,36 +1,18 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../config/connection.js");
-
-var burger = {
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
-    insertOne: function (vals, cb) {
-        orm.insertOne("burgers", "burger_name", vals, function (res) {
-            cb(res);
-        });
-    },
-    updateOne: function (colNewVal, condition, cb) {
-        orm.updateOne("burgers", colNewVal, condition, function (res) {
-            cb(res);
-        });
-    }
-};
-
-const Burger = sequelize.define("burger", {
-    burgerName: {
-        type: DataTypes.STRING,
-        validate: {
-            notNull: true,
-            is: /^[a-z0-9\s]+$/i
+module.exports = function(sequelize, DataTypes) {
+    const Burger = sequelize.define("burgers", {
+        burgerName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                is: /^[a-z0-9\s]+$/i
+            },
+            field: 'burger_name'
         },
-        field: 'burger_name'
-    },
-    devoured: DataTypes.BOOLEAN
-})
-
-Burger.sync();
-
-module.exports = Burger;
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    })
+    return Burger;
+};
